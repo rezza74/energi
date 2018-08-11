1cb67993041bb80c70d3129203c0116aaa9e440a
Andrey Galkin <andrey@futoin.org>
2018-08-11 10:42:51 +0300

    FIXED: autogen.sh for windows cross-build

--- autogen.sh
+++ autogen.sh
@@ -14,7 +14,7 @@ elif which apt-get >/dev/null 2>&1; then
     deb_list="${deb_list} python-pip python-setuptools python-dev"
     deb_list="${deb_list} python3-pip python3-setuptools python3-dev"
     deb_list="${deb_list} build-essential g++ libtool autotools-dev automake bsdmainutils pkg-config"
-    deb_list="${deb_list} autoconf autoconf2.*"
+    deb_list="${deb_list} autoconf autoconf2.13 autoconf2.64"
     deb_list="${deb_list} libssl-dev libevent-dev"
     deb_list="${deb_list} libboost-system-dev libboost-filesystem-dev libboost-chrono-dev"
     deb_list="${deb_list} libboost-program-options-dev libboost-test-dev libboost-thread-dev"
@@ -28,7 +28,14 @@ elif which apt-get >/dev/null 2>&1; then
     deb_list="${deb_list} clang-5.0"
     
     if [ "$HOST" = "x86_64-w64-mingw32" ]; then
-        deb_list="${deb_list} mingw-w64 wine64 wine-binfmt"
+        deb_list="${deb_list} mingw-w64"
+        
+        if [ "$(lsb_release -rs)" = "16.04" ]; then
+            # old CI node
+            deb_list="${deb_list} wine64-development"
+        else
+            deb_list="${deb_list} wine64 wine-binfmt"
+        fi
     fi
     
     deb_to_install=""
@@ -70,5 +77,6 @@ autoreconf --install --force --warnings=all $srcdir
 if [ "$HOST" = "x86_64-w64-mingw32" ]; then
     echo "Preparing Win64 deps"
     make -C $srcdir/depends HOST=x86_64-w64-mingw32 -j${MAKEJOBS:-$(nproc)}
+    cp -rau $srcdir/depends/x86_64-w64-mingw32/* $srcdir/build/current/
 fi
 
